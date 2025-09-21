6bb922d53ba5be138a9cc16df79f12f20bfa5d9a
ekaterinagevorkova <ekaterinagevorkova622@gmail.com>
2025-09-21 23:31:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ import zipfile
 st.set_page_config(page_title="Internal tools", layout="wide")
 
 st.markdown("<h1 style='color:#28EBA4;'>КОНВЕРТАЦИЯ В HTML</h1>", unsafe_allow_html=True)
+
 # -------------------- ГЕНЕРАТОР СЛАГОВ -------------------- #
 st.markdown("<h1 style='color:#28EBA4;'>СЛАГИ ДЛЯ ССЫЛОК</h3>", unsafe_allow_html=True)
 words_raw = st.text_input("2–3 слова для слага (через пробел / запятую)", key="slug_words", placeholder="пример: гол статистика")
