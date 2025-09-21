6307aff8d4db1a280ead34e28f08c56819d094d4
ekaterinagevorkova <ekaterinagevorkova622@gmail.com>
2025-09-21 23:53:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ import re
 from itertools import product, permutations
 
 st.set_page_config(page_title="Internal tools", layout="wide")
+st.markdown(
+    "<div style='text-align: center; margin-bottom: 20px;'>"
+    "<img src='https://dumpster.cdn.sports.ru/7/f5/0180499259d7b11aaceee78e2b91e.png' width='80'>"
+    "</div>",
+    unsafe_allow_html=True
+)
 
 col1, col2 = st.columns(2)
 
