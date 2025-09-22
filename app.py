188a3207c0765f9b20391ca4295132eef4f582dc
ekaterinagevorkova <ekaterinagevorkova622@gmail.com>
2025-09-22 12:24:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,74 +7,14 @@ import pandas as pd
 import re
 from itertools import product, permutations
 
-col1, col2 = st.columns(2)
 st.set_page_config(page_title="Internal tools", layout="wide")
-
 st.markdown(
-    """
-    <div style='
-        text-align: center; 
-        margin-top: -40px;   /* двигаем выше */
-        margin-bottom: 20px;
-        position: relative;
-        top: 0;
-    '>
-        <img src='https://dumpster.cdn.sports.ru/7/f5/0180499259d7b11aaceee78e2b91e.png' width='80'>
-    </div>
-    """,
+    "<div style='text-align: center; margin-bottom: 20px;'>"
+    "<img src='https://dumpster.cdn.sports.ru/7/f5/0180499259d7b11aaceee78e2b91e.png' width='80'>"
+    "</div>",
     unsafe_allow_html=True
 )
-
-
-# Цвет фона
-# Цвет фона
-st.markdown("""
-<style>
-[data-testid="stAppViewContainer"] { background-color:#0E1117 !important; color:#FAFAFA !important; }
-[data-testid="stHeader"]{ background:transparent !important; }
-[data-testid="stSidebar"]{ background-color:#0E1117 !important; color:#FAFAFA !important; }
-
-/* общий текст */
-label, p, span { color:#FAFAFA !important; }
-
-/* заголовки мятные */
-h1, h2, h3, h4, h5, h6 { color:#28EBA4 !important; }
-
-input, textarea { color:#FAFAFA !important; background-color:#11151c !important; border-color:#2a2f3a !important; }
-.stDownloadButton > button, button[kind="primary"], button[kind="secondary"] { background-color:#28EBA4 !important; color:#0E1117 !important; border-color:#28EBA4 !important; }
-.block-container { padding-top: 1rem; }
-</style>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-<style>
-/* инпуты и выпадающие списки */
-input, textarea, select, .stTextInput input, .stSelectbox div[data-baseweb="select"] > div {
-    background-color: #1E222A !important;  /* тёмно-серый фон */
-    color: #FAFAFA !important;             /* белый текст */
-    border: 1px solid #444 !important;
-}
-
-/* placeholder тоже белый */
-input::placeholder, textarea::placeholder {
-    color: #AAAAAA !important;
-}
-
-/* file uploader */
-[data-testid="stFileUploader"] section {
-    background-color: #1E222A !important;
-    color: #FAFAFA !important;
-    border: 1px dashed #444 !important;
-}
-
-/* select (выпадающий список) текст */
-.stSelectbox div[data-baseweb="select"] span {
-    color: #FAFAFA !important;
-}
-</style>
-""", unsafe_allow_html=True)
-
-
+col1, col2 = st.columns(2)
 
 # ==================== ЛЕВАЯ КОЛОНКА ==================== #
 with col1:
@@ -318,4 +258,5 @@ with col2:
             st.text_area("Варианты слагов", value=text_blob, height=200)
         else:
             st.caption("Введите от 2 до 3 слов.")
+
 
